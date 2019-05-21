1dcecb73156391f92ea0693a786ea477f863cb14
aldy0 <50771648+aldy0@users.noreply.github.com>
2019-05-21 09:46:15 +0700

    Update edit-user.php

--- simpanpinjambank/edit-user.php
+++ simpanpinjambank/edit-user.php
@@ -1,7 +1,6 @@
 <?php
 include ("config.php");
 
- 
 $id = $_GET['id']; // id berasal dari url
 $query = "SELECT * FROM login WHERE id = $id";
 $data = mysqli_query($db, $query);
@@ -17,7 +16,7 @@ $data = mysqli_query($db, $query);
 <body>
 
     <!-- navbar -->
-        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <nav class="navbar navbar-inverse navbar-fixed-top">
             <div class="container-fluid">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -31,30 +30,31 @@ $data = mysqli_query($db, $query);
    
             </nav>
      <!-- akhir navbar-->
-     <br><br><br><br>
-     <div class="container">
+         
+    <br><br><br><br>
+    <div class="container">
     <h3>Edit User</h3>
     <?php while($row = mysqli_fetch_assoc($data)){ ?>
     <form action="proses-update-user.php" method="post">
         username : <input type="text" name="username" value="<?php echo $row['username']; ?>"><br><br>
-         password : <input type="text" name="password" value="<?php echo $row['password']; ?>"><br><br>
-          level: <input type="text" name="level" value="<?php echo $row['level']; ?>"><br><br>
-
-        
-        
+        password : <input type="text" name="password" value="<?php echo $row['password']; ?>"><br><br>
+        level: <input type="text" name="level" value="<?php echo $row['level']; ?>"><br><br>
+     
         <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
         <input type="submit" name="submit" value="Update Data">
     </form>
     </div>
+         
     <?php
     } // end while
 
     ?>
 
-     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-     <script src="js/jquery.min.js"></script>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.min.js"></script>
     <script src="js/jquery.easing.1.3.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
+         
 </body>
 </html>
